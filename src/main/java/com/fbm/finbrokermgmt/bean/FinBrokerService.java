210882f596db5776f1bb78d0bf10bdb40ab405c7
Gaurav Sharma <21275839+gsharma243@users.noreply.github.com>
2019-12-07 19:18:51 +0530

    more changes

--- src/main/java/com/fbm/finbrokermgmt/bean/FinBrokerService.java
+++ src/main/java/com/fbm/finbrokermgmt/bean/FinBrokerService.java
@@ -1,10 +1,18 @@
 package com.fbm.finbrokermgmt.bean;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class FinBrokerService {
+	
+	public void addUser(Map<String, String[]> userMap) {
+		
+	}
 
-	
-	
+	public void addDeal(Map<String, String[]> dealMap) {
+		
+	}
+
 }
